0072459b0c5b8649d2ded8fa5a10cb737fbcaaad
Nathank4242 <nkweon@gmail.com>
2025-01-31 14:01:38 -0500

    vue

---
+++ app/src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<script>
+import Thingy from '../components/Thingy.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="about flex justify-center bg-cyan-200">
+    <h1>Welcome to the page</h1>
+  </div>
+  <RouterView />
+</template>
+
+<style>
+body {
+  background-color: black;
+}
+nav {
+  font-size: 12px;
+}
+</style>
