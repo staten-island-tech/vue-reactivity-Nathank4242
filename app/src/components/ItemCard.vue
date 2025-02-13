53e48f773ea5138fbda2ee67ec4fbb82810806ba
Nathank4242 <nkweon@gmail.com>
2025-02-13 14:02:39 -0500

    vue

--- app/src/components/ItemCard.vue
+++ app/src/components/ItemCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="card border-indigo-600 box-border min-h-max w-64 p-4 border-8 m-8">
+  <div class="card border-indigo-600 box-border min-h-max w-full sm:w-64 p-4 border-8 m-8">
     <h2 class="card-name text-blue-600 text-xl">{{ item.name }}</h2>
-    <img :src="item.image" alt="" />
+    <img :src="item.image" alt="" class="w-full h-auto" />
     <h3 class="card-name text-blue-600 text-xl">{{ item.price }}</h3>
     <slot></slot>
   </div>
--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -14,29 +14,28 @@ import items from '../assets/data.json'
     <h1>Welcome to Pokemon Center Item Shop</h1>
   </div>
   <RouterView />
-  <button
-    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-  >
-    Nothing button
-  </button>
-
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-    <ItemCard v-for="item in items" :key="item.name" :item="item" @click="console.log(item.name)"
-      ><button
-        type="button"
-        class="btn bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700"
-        id="add"
-        @click="console.log(item.name)"
-      >
-        Add To Cart
-      </button></ItemCard
+  <div class="flex flex-row">
+    <!-- Items Grid -->
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 w-full pr-80"
     >
-  </div>
+      <ItemCard v-for="item in items" :key="item.name" :item="item">
+        <button
+          type="button"
+          class="btn bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700 w-full"
+          id="add"
+          @click="console.log(item.name)"
+        >
+          Add To Cart
+        </button>
+      </ItemCard>
+    </div>
 
-  <div
-    class="fixed top-0 right-0 w-80 h-full bg-gray-100 border-l-4 border-gray-600 p-4 overflow-y-auto"
-  >
-    <h1 class="text-center text-2xl font-bold mb-6">Cart</h1>
+    <div
+      class="cart fixed top-0 right-0 w-80 h-full bg-gray-100 border-l-4 border-gray-600 p-4 overflow-y-auto z-10"
+    >
+      <h1 class="text-center text-2xl font-bold mb-6">Cart</h1>
+    </div>
   </div>
 </template>
 
